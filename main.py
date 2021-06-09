1d3db974a4b00041792a99b0ae04a5d5f236d11f
Raian P. B <57734572+raianb-dev@users.noreply.github.com>
2021-06-09 04:30:24 +0000

    Finder e-mails

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from Class import classe
+from pyhunter import PyHunter as ph
 
 
+api = ph('my_hunter_api_key')
 
+#Finder All e-mails
+comp = str(input('Company :'))
+email = api.domain_search(company=f'{comp}')
 
-classe.Conection()
-
-
-
-
-
-
+df = classe.Data(email)
+df.exibir()
