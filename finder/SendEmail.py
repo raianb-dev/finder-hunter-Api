c039a245c52804bf7e625250b84b5c40329e9bb6
Raian P. B <57734572+raianb-dev@users.noreply.github.com>
2021-06-14 16:42:32 +0000

    update SendEmail

--- finder/SendEmail.py
+++ finder/SendEmail.py
@@ -1,21 +1,52 @@
+
 import smtplib
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-Sender_Email = input("Seu email: ")
-Password = input('Password: ')
-Reciever_Email = input("Remetente: ")
+# Configuração
+host = 'smtp.gmail.com'
+port = 587
+user = ''
+password = ''
 
+# Criando objeto
+print('Criando objeto servidor...')
+server = smtplib.SMTP(host, port)
 
-newMessage = EmailMessage()                         
-newMessage['Subject'] = "Sua tabela de Csv." 
-newMessage['From'] = Sender_Email                   
-newMessage['To'] = Reciever_Email                   
-newMessage.set_content('Segue abaixo o anexo da sua tabele de emails') 
+# Login com servidor
+print('Logando...')
+server.ehlo()
+server.starttls()
+server.login(user, password)
 
-files = ['DropboxRelatorio.csv']
+# Criando mensagem
+message = 'Olá, mundo!'
+print('Criando mensagem...')
+email_msg = MIMEMultipart()
+email_msg['From'] = user
+email_msg['To'] = ''
+email_msg['Subject'] = 'Assunto da mensagem'
+print('Adicionando texto...')
+email_msg.attach(MIMEText(message, 'plain'))
 
+print('Obtendo arquivo...')
+filename = 'Anoexo' 
+filepath = '../out/Instagram-emails.csv'
+attachment = open(filepath, 'rb')
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    
-    smtp.login(Sender_Email, Password)              
-    smtp.send_message(newMessage)   
+print('Lendo arquivo...')
+att = MIMEBase('application', 'octet-stream')
+6*encoders.encode_base64(att)
+att.add_header('Content-Disposition', f'attachment; filename= {filename}')
+
+attachment.close()
+print('Adicionando arquivo ao email...')
+email_msg.attach(att)
+
+# Enviando mensagem
+print('Enviando mensagem...')
+server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+print('Mensagem enviada!')
+server.quit()
